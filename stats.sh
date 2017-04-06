5bb72f0dbf8cd0e6a7c6eaeebabac3095b1c6bc2
Diego Diez <diego10ruiz@gmail.com>
2017-04-06 19:57:03 +0900

    Generalize stats.sh

--- stats.sh
+++ stats.sh
@@ -1,1 +1,8 @@
-curl https://hub.docker.com/v2/repositories/ddiez/hmmer/
+repo=$1
+if [ "$repo" == "" ]; then
+  repo="ddiez/biodev"
+  echo "Checking stats for $repo ..."
+  echo
+fi
+
+curl https://hub.docker.com/v2/repositories/$repo/
